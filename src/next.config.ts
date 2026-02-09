08887817627dd633b4fad2b21036ba796f7e6b86
sd22404 <sd22404@bristol.ac.uk>
2026-02-09 13:43:24 +0000

    fix asset path

--- src/next.config.ts
+++ src/next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  images: {
-	unoptimized: true,
-  },
+  images: { unoptimized: true },
+  basePath: "/COMSM0052",
+  assetPrefix: "/COMSM0052/",
 };
 
 export default nextConfig;
